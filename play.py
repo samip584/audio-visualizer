dbecc1a125527c95bb139024cf2b3ee9b77ab4c6
Samip <samip584>
2019-05-07 20:53:30 +0545

    1st

--- play.py
+++ play.py
@@ -3,7 +3,7 @@ import wave
 
 import os
 import sys
-import time
+import timeit
 
 import struct
 import numpy as np
@@ -19,7 +19,7 @@ from tkinter import *
 
 #import pandas as pd
 # constants
-CHUNK = 1024
+CHUNK = int(1024 * 4)
 FORMAT = pyaudio.paInt16 # has smth to do with bites per sec
 CHANNELS = 1
 
@@ -27,18 +27,18 @@ CHANNELS = 1
 root = Tk()
 root.minsize(300,250)
 
-pygame.mixer.init()
-pygame.mixer.music.load("Johnny3.wav")
-pygame.mixer.music.play()
+#pygame.mixer.init()
+#pygame.mixer.music.load("Johnny3.wav")
+#pygame.mixer.music.play()
 
 
-
-
-wf = wave.open("Johnny3.wav", 'rb')
+wf = wave.open("Johnny1.wav", 'rb')
 
 total_frame = wf.getnframes()
 total_cycle = total_frame/1024
-
+RATE = wf.getframerate()
+length = total_frame/RATE
+print(length)
 #pyaudio class instance
 p = pyaudio.PyAudio()
 
@@ -46,7 +46,7 @@ p = pyaudio.PyAudio()
 # open stream 
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 channels=wf.getnchannels(),
-                rate=wf.getframerate(),
+                rate=RATE,
                 output = True,
 				frames_per_buffer = CHUNK
 				)
@@ -56,32 +56,32 @@ fig = plt.figure.Figure()
 ax = fig.add_subplot(111)
 canvas = FigureCanvasTkAgg(fig, master=root)
 
-x = np.arange(0, int(CHUNK/2), 2)
-line, = ax.plot(x, np.random.rand(int(CHUNK/4)))
-ax.set_ylim(-250, 250)
-ax.set_xlim(0, int(CHUNK/4))
+x = np.linspace(0, RATE, int(CHUNK))
+line, = ax.semilogx(x, np.random.rand(int(CHUNK)))
+ax.set_xlim(20, RATE/2)
 ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
+ax.yaxis.set_ticklabels([])
 ax.set_yticklabels([])
 ax.set_xticklabels([])
 
 data = wf.readframes(CHUNK)
-start_time = time.time()
 # play stream (3)
 show = 0
+
+
+
 while len(data) > 0:
 	data = wf.readframes(CHUNK)
-	new_data = np.array(struct.unpack(str(CHUNK * 2) + 'B', data), dtype='b')[::8]
-	#data_int = np.concatenate((data_int, new_data))
-	if show == 1:
-		line.set_ydata(new_data)
-		fig.canvas.draw()
-		fig.canvas.flush_events()
-		canvas.show()
-		canvas.get_tk_widget().pack(fill = BOTH, expand = 1)
-	show = (show + 2)%3
-
-
+	new_data = np.fromstring(data, dtype=np.int16)  
+	y_fft = fft(new_data)
+	#data_int = np.concatenate((data_int, new_data))	
+	stream.write(data)
+	line.set_ydata(np.abs(y_fft[0:CHUNK])*2 / (10000 * CHUNK)) 
+	#line.set_ydata(new_data)
+	fig.canvas.draw()
+	fig.canvas.flush_events()
+	canvas.show()
+	canvas.get_tk_widget().pack(fill = BOTH, expand = 1)
 
 
 
